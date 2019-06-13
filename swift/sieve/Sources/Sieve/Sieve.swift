b59fac7b397d7ad9ac06ece866d31a33800be24d
Z-Ean Chung <zchung@us.ci.org>
2019-06-12 20:42:12 -0600

    Completed sieve - no idea

--- swift/sieve/Sources/Sieve/Sieve.swift
+++ swift/sieve/Sources/Sieve/Sieve.swift
@@ -1,25 +1,29 @@
+import Foundation
+
+//this version (without NSMutableSet) ran Sieve(1000).primes in 26.319 seconds
+
 struct Sieve {
-    var limit: Int
-    init(_ limit: Int) {
-        self.limit = limit
-    }
-    
-    var primes: [Int] {
-        if limit < 2 { return [] }
-        
-        var primeNumbers = [Int]()
-        var marked = [Int: Bool]()
-        for index in 2...limit {
-            if(marked[index] == nil) {
-                let multiples = limit / index
-                for multiplier in 0...multiples {
-                    let mutliple = multiplier*index
-                    marked[mutliple] = true
-                }
-                primeNumbers.append(index)
+    var primes = [Int]()
+
+    private func sieveForPrimes(_ limit:Int) -> [Int] {
+        var composites = [Int]()
+        var primes = [Int]()
+
+        for i in 2..<limit {
+            if !composites.contains(i) {
+                primes.append(i)
+            }
+
+            var counter = 2
+            while i * counter < limit {
+                composites.append(i * counter)
+                counter += 1
             }
         }
-        
-        return primeNumbers
+        return primes
+    }
+
+    init(_ input:Int) {
+        self.primes = sieveForPrimes(input)
     }
 }
