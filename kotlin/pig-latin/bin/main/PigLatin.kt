b4547c893ff115a12ef52d32ab65ae255aa91012
Z-Ean Chung <zchung@us.ci.org>
2019-06-15 17:43:17 -0600

    solve piglatin

--- kotlin/pig-latin/bin/main/PigLatin.kt
+++ kotlin/pig-latin/bin/main/PigLatin.kt
@@ -1,18 +1,15 @@
 object PigLatin {
-    fun translate(str: String): String {
-
-        if(str.isEmpty()) { return "" }
-
-        str.split(" ").forEach {
-            var result = ""
-        }.joinToString(" ")
-
-        val first = str.first()
-
-        // when (first) {
-        //     'a', 'e', 'i', 'o', 'u' -> { result = "${str}ay" }
-        //     else -> { result = str.substring(1) + first + "ay" }
-        // }
-        // return result
-    }
+  fun translate(phrase: String) =
+    phrase.split(" ")
+          .map { word ->
+          when {
+            word.first().isVowel() -> word + "ay"
+            listOf("yt", "xr").contains(word.take(2)) -> word + "ay"
+            listOf("sch", "thr", "squ").contains(word.take(3)) -> word.takeLast(word.length - 3) + word.take(3) + "ay"
+            listOf("ch", "sh", "qu", "th", "rh").contains(word.take(2)) -> word.takeLast(word.length - 2) + word.take(2) + "ay"
+            else -> word.takeLast(word.length - 1) + word.first() + "ay"
+          }
+      }.joinToString(separator = " ")
 }
+
+fun Char.isVowel() = listOf('a', 'e', 'i', 'o', 'u').contains(this)
--- swift/space-age/Sources/SpaceAge/SpaceAge.swift
+++ swift/space-age/Sources/SpaceAge/SpaceAge.swift
@@ -4,44 +4,35 @@ class SpaceAge {
 
     let seconds: TimeInterval
     var onEarth: Double {
-        get {
-            return ageOnPlanet(withPeriod: 1)
-        }
+        return ageOnPlanet(withPeriod: 1)
     }
+
     var onMercury: Double {
-        get {
-            return ageOnPlanet(withPeriod: 0.2408467)
-        }
+         return ageOnPlanet(withPeriod: 0.2408467)
     }
+
     var onJupiter: Double {
-        get {
-            return ageOnPlanet(withPeriod: 11.862615)
-        }
+        return ageOnPlanet(withPeriod: 11.862615)
     }
+
     var onMars: Double {
-        get {
-            return ageOnPlanet(withPeriod: 1.8808158)
-        }
+        return ageOnPlanet(withPeriod: 1.8808158)
     }
+
     var onVenus: Double {
-        get {
-            return ageOnPlanet(withPeriod: 0.61519726)
-        }
+        return ageOnPlanet(withPeriod: 0.61519726)
     }
+
     var onSaturn: Double {
-        get {
-            return ageOnPlanet(withPeriod: 29.447498)
-        }
+        return ageOnPlanet(withPeriod: 29.447498)
     }
+    
     var onUranus: Double {
-        get {
-            return ageOnPlanet(withPeriod: 84.016846)
-        }
+        return ageOnPlanet(withPeriod: 84.016846)
     }
+
     var onNeptune: Double {
-        get {
-            return ageOnPlanet(withPeriod: 164.79132)
-        }
+        return ageOnPlanet(withPeriod: 164.79132)
     }
 
     init(_ ageInSeconds: Double) {
