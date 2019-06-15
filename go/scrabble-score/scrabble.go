b43375cdce63f57429969a409f804f600eef6adf
Z-Ean Chung <zchung@us.ci.org>
2019-06-15 11:55:50 -0600

    Changed scrable

--- go/scrabble-score/scrabble.go
+++ go/scrabble-score/scrabble.go
@@ -2,6 +2,37 @@ package scrabble
 
 import "strings"
 
+// scoreMap represents a map of the score of a rune
+var scoreMap = map[rune]int {
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2, 
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
+}
+
+// Score returns the total score of the given string value
 func Score(str string) int {
 	score := 0
 
@@ -11,23 +42,16 @@ func Score(str string) int {
 	return score
 }
 
+// getValue returns the specific score of a given character
 func getValue(str string) int {
 	value := 0
-	switch str {
-	case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
-		value = 1
-	case "D", "G":
-		value = 2
-	case "B", "C", "M", "P":
-		value = 3
-	case "F", "H", "V", "W", "Y":
-		value = 4
-	case "K":
-		value = 5
-	case "J", "X":
-		value = 8
-	case "Q", "Z":
-		value = 10
+	for k, v := range scoreMap {
+
+		if string(k) == str {
+			value = v
+			break
+		}
 	}
+
 	return value
 }
