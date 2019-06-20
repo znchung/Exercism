81c4c66819e0dc34880e747dc7623e8e145e2c70
Z-Ean Chung <zchung@us.ci.org>
2019-06-20 08:08:19 -0600

    Solved bob

--- swift/bob/Sources/Bob/Bob.swift
+++ swift/bob/Sources/Bob/Bob.swift
@@ -1,1 +1,27 @@
-//Solution goes in Sources
+import Foundation
+
+class Bob {
+    static func hey(_ input:String) -> String {
+        switch true {
+            case isShouting(input): return "Whoa, chill out!"
+            case isSilence(input): return "Fine. Be that way!"
+            case isAskingQuestion(input): return "Sure."
+            default: return "Whatever."
+        }
+    }
+
+    private static func isShouting(_ input:String) -> Bool {
+        let hasLowercaseLetters = input != input.uppercased()
+        let hasUppercaseLetters = input != input.lowercased()
+        return hasUppercaseLetters && !hasLowercaseLetters
+    }
+    
+    private static func isSilence(_ input:String) -> Bool {
+        let inputWithoutWhitespace = input.trimmingCharacters(in: .whitespacesAndNewlines)
+        return inputWithoutWhitespace == ""
+    }
+    
+    private static func isAskingQuestion(_ input:String) -> Bool {
+        return input.hasSuffix("?")
+    }
+}
