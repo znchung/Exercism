ad292c9f05168eab90596d6e903d23771e0eede4
Z-Ean Chung <zchung@us.ci.org>
2019-07-01 07:43:19 -0600

    Fixed grains - swift

--- swift/grains/Sources/Grains/Grains.swift
+++ swift/grains/Sources/Grains/Grains.swift
@@ -1,23 +1,18 @@
 import Foundation
 
-class Grains {
+enum GrainsError: Error {
+    case inputTooHigh(_ message: String)
+    case inputTooLow(_ message: String)
 
-    enum GrainsError: Error {
-        case inputTooHigh(_ message: String)
-        case inputTooLow(_ message: String)
-
-        init(_ num: Int) {
-            let errorString = "Input[\(num)] invalid. Input should be between 1 and 64 (inclusive)"
-            self = num > 64 ? .inputTooHigh(errorString) : .inputTooLow(errorString)
-        }
+    init(_ num: Int) {
+        let errorString = "Input[\(num)] invalid. Input should be between 1 and 64 (inclusive)"
+        self = num > 64 ? .inputTooHigh(errorString) : .inputTooLow(errorString)
     }
+}
 
-    static let total: UInt = UInt.max
+let total: UInt = UInt.max
 
-    static func square(_ num: Int) throws -> UInt {
-
-        guard (1...64).contains(num) else { throw GrainsError(num) }
-
-        return UInt(pow(2.0, Double(num-1)))
-    }
+func square(_ num: Int) throws -> UInt {
+    guard (1...64).contains(num) else { throw GrainsError(num) }
+    return UInt(pow(2.0, Double(num-1)))
 }
