24154be655fad40f58ce505eefe4a65fc9c57a59
Z-Ean Chung <zchung@us.ci.org>
2019-07-17 21:13:11 -0600

    kotlin word-count

---
+++ kotlin/word-count/bin/main/WordCount.kt
@@ -0,0 +1,9 @@
+object WordCount {
+    fun phrase(input: String): Map<String, Int> {
+        return input.toLowerCase()
+                    .split("\'?[^a-z0-9\']+\'?".toRegex())
+                    .filter( String::isNotEmpty)
+                    .groupBy { it }
+                    .mapValues({ it.value.size })
+    }
+}
